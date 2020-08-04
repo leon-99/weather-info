6f20849d1b4079c83af5e7a6313b1a21f85bbd16
leon-99 <leonzifer@gmail.com>
2020-08-04 19:37:44 +0630

    UI Changed

--- src/dataVue.js
+++ src/dataVue.js
@@ -6,7 +6,7 @@ export const dataVue = {
     localTime: null, 
     infoTexts: false,
     loading: true,
-    bgImage: 'cloudy-d',
+    bgImage: 'cloudy-n',
     detailDataTexts: true,
     aqiColor: '',
     alertTitleText: false,
